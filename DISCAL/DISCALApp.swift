0cb199242bf2b61520c89a1a62d3d90b17dc6dcc
JISU-YANG <jisuyang@egoist.im>
2023-02-17 16:11:49 +0900

    Initial Commit

---
+++ DISCAL/DISCALApp.swift
@@ -0,0 +1,17 @@
+//
+//  DISCALApp.swift
+//  DISCAL
+//
+//  Created by JISU YANG on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct DISCALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
